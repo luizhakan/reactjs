dad2a5266a7ae7ec403de510c55e0cb12e14c3a1
luizhakan <uefibiosxx@gmail.com>
2022-06-10 10:10:14 -0300

    React Hour Code - Costs 10/06/2022 - O projeto continua indo bem, e eu continuo deixando o backend em conta do professor, eu tô procurando dar uma melhorada na questão de estilização.

--- react-hour-code/costs/src/components/pages/Projetos.jsx
+++ react-hour-code/costs/src/components/pages/Projetos.jsx
@@ -1,7 +1,15 @@
+import { useLocation } from "react-router-dom";
+
+import Message from "../layout/Message";
+
 export default function Projetos(){
+    const location = useLocation();
+    let message = "";
+
     return(
         <div>
-            <h1>Projetos</h1>
+            <h1>Meus projetos</h1>
+            <Message msg="Alguma mensagem" type="sucess" />
         </div>
     )
 }
