65da43a0b115da6f06f1881591d801ddb8f6b330
luizhakan <uefibiosxx@gmail.com>
2022-06-16 21:39:12 -0300

    React Journey 6 - uuid and useReducer 16/06/2022 - Aproveitando o repositório do useReducer para usar o uuid do qual eu tanto ouvi falar, usado para resolver o problema do unique key prop que dá no react.

--- react-journey/react006-reducer/src/App.jsx
+++ react-journey/react006-reducer/src/App.jsx
@@ -1,9 +1,22 @@
-import { useReducer } from "react";
+import { useState } from "react";
+import Nomes from "./components/Nomes";
 
 export default function App() {
-  return(
+  const [nome, setNome] = useState('');
+  const [nomes, setNomes] = useState([]);
+
+  function guardarNome(){
+    setNomes(tmp => [...tmp, nome]);
+  }
+
+  return (
     <>
-    
+      <h3>React Unique ID</h3>
+      <hr />
+      
+      <input type="text" onChange={e => {setNome(e.target.value)}} value={nome} />
+      <button onClick={guardarNome}>Guardar Nome</button>
+      <Nomes nomes={nomes}/>
     </>
-  )
+  );
 }
