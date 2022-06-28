a3f21e17d7d1e8597d573b72cf7694e45fe91a5e
luizhakan <uefibiosxx@gmail.com>
2022-06-27 21:15:41 -0300

    React Journey 9 - Bootstrap 27/06/2022 - Uma maneira de adicionar Bootstrap em um projeto que eu não conhecia, foi interessante. Links: react.bootstrap.github.io , reactstrap.github.io . Para adicionar o bootstrap no projeto, é necessário um "npm install bootstrap"

---
+++ react-journey/react009-bootstrap/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <div>
+      <h1>React - Adicionando Bootstrap</h1>
+      <hr />
+      <button className="btn btn-dark">Botão Bootstrap</button>
+      <div className="alert alert-success">Feito!</div>
+    </div>
+  )
+}
+
+export default App
